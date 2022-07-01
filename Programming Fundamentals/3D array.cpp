2481f1ded9100da46134fd5bd95d1c2aefdfd65a
Taimour Abdul Karim <86923612+Tkarim45@users.noreply.github.com>
2022-07-01 22:59:09 +0500

    Update 3D array.cpp

--- Programming Fundamentals/3D array.cpp
+++ Programming Fundamentals/3D array.cpp
@@ -1,11 +1,12 @@
+// Allocation of 3D Array and Data is Stored in 3D Arrays 
 #include<iostream>
 using namespace std;
 int main()
 {
-	int maxrow=3;
-	int maxcol=3;
+	// Declaration of 3D Array
 	int test[2][3][2]={{{1,2},{3,4},{5,6}},	{{7,8},{9,10},{11,12}}};
 	
+	// Loop to print Data and to Show how and where data is Stored (Index at which data is stored)
 	for(int i=0;i<2;i++)
 	{
 		for(int j=0;j<3;j++)
